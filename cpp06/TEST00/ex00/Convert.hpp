8f71221c1228a937c553f8fd54b2b33089665fde
zaayddev <zchbani@student.1337.ma>
2022-11-05 05:46:27 +0100

    class modif

--- cpp06/TEST00/ex00/Convert.hpp
+++ cpp06/TEST00/ex00/Convert.hpp
@@ -1,5 +1,6 @@
 #ifndef CONVERT_HPP
 # define CONVERT_HPP
+
 # include <iostream>
 # include <iomanip>
 # include <string>
@@ -10,48 +11,47 @@
 
 class Convert
 {
-    public:
-        /* Constructors and Destructors */
-        Convert(void);
-        Convert(std::string input);
-        Convert(const Convert& rhs);
-        ~Convert(void);
-        /* Operators */
-        Convert& operator=(const Convert& rhs);
-        /* Exceptions */
-        class InvalidInputException: public std::exception
-        {
-            public:
-                const char* what(void) const throw();
-        };
-        /* Getter */
-        std::string getInput(void) const;
-        char        getChar(void) const;
-        int         getInt(void) const;
-        float       getFloat(void) const;
-        double      getDouble(void) const;
-        /* Setter */
-        void        setInput(std::string input);
-        void        setChar(char c);
-        void        setInt(int i);
-        void        setFloat(float f);
-        void        setDouble(double d);
-        /* Booleans */
-        bool        isChar(std::string input);
-        bool        isInt(std::string input);
-        bool        isFloat(std::string input);
-        bool        isDouble(std::string input);
-        bool        isOther(std::string input);
-        bool        isIntOverflow(void);
-        /* Conversions */
-        void        detectTypeAndConversion(void);
-
     private:
         std::string _input;
         char        _c;
         int         _i;
         float       _f;
         double      _d;
+
+    public:
+        Convert();
+        Convert(std::string input);
+        Convert(const Convert& rhs);
+        ~Convert();
+
+        Convert& operator=(const Convert& rhs);
+
+        class InvalidInputException: public std::exception
+        {
+            public:
+                const char* what() const throw();
+        };
+
+        std::string getInput() const;
+        char        getChar() const;
+        int         getInt() const;
+        float       getFloat() const;
+        double      getDouble() const;
+
+        void        setInput(std::string input);
+        void        setChar(char c);
+        void        setInt(int i);
+        void        setFloat(float f);
+        void        setDouble(double d);
+
+        bool        isChar(std::string input);
+        bool        isInt(std::string input);
+        bool        isFloat(std::string input);
+        bool        isDouble(std::string input);
+        bool        isOther(std::string input);
+        bool        isIntOverflow();
+
+        void        detectTypeAndConversion();
 };
 
 #endif
