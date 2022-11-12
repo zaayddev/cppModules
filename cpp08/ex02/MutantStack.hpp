c03390d467516ba090ca9eb41b74d80c7093a705
zaayddev <zchbani@student.1337.ma>
2022-11-12 22:23:21 +0100

    add

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <stack>
+
+template<typename T, typename Container = std::deque<T> >
+class	MutantStack : public std::stack<T, Container>
+{
+	public:
+
+		MutantStack(void) {};
+		MutantStack(MutantStack const &src) : MutantStack<T, Container>::stack(src) {};
+		~MutantStack(void) {};
+
+		MutantStack	&operator=(MutantStack const &rhs) {
+			this->MutantStack::stack::operator=(rhs);
+			return (*this);
+		}
+
+		typedef typename Container::iterator	iterator;
+
+		iterator	begin(void) {
+			return (this->c.begin());
+		}
+
+		iterator	end(void) {
+			return (this->c.end());
+		}
+};
